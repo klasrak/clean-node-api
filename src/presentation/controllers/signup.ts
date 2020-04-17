d1fec78b498aac0b3870f09df7d5a0b15e6bc3c8
Danilo Augusto <dasf_cm@terra.com.br>
2020-04-17 17:14:27 -0300

    refactor: move all imports to single files

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,11 @@
-import { HttpRequest, HttpResponse } from '../protocols/http';
-import { MissingParamError } from '../errors/missing-param-error';
-import { badRequest, serverError } from '../helpers/http-helper';
-import { Controller } from '../protocols/controller';
-import { EmailValidator } from '../protocols/emai-validator';
-import { InvalidParamError } from '../errors/invalid-param-error';
+import { InvalidParamError, MissingParamError } from '../errors';
+import { badRequest, serverError } from '../helpers';
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse,
+} from '../protocols';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
